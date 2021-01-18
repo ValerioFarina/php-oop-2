23d99b9e472d83ae01f07b8e5cc56e09a0e26ea3
Valerio Farina <valerio.farina.1994@gmail.com>
2021-01-18 15:14:32 +0100

    Spostata in un file a parte la definizione della classe User

--- index.php
+++ index.php
@@ -1,54 +1,5 @@
 <?php
-    class User {
-        protected $first_name;
-        protected $last_name;
-        protected $email;
-        protected $age;
-        protected $gender;
-        protected $nationality;
-
-        function __construct($_first_name, $_last_name, $_email, $_age = null, $_gender = "", $_nationality = "") {
-            $this->first_name = $_first_name;
-            $this->last_name = $_last_name;
-            $this->email = $_email;
-            $this->age = $_age;
-            $this->gender = $_gender;
-            $this->nationality = $_nationality;
-        }
-
-        public function getFirstName() {
-            return $this->first_name;
-        }
-
-        public function getLastName() {
-            return $this->last_name;
-        }
-
-        public function getEmail() {
-            return $this->email;
-        }
-
-        public function getAge() {
-            return $this->age;
-        }
-
-        public function getGender() {
-            return $this->gender;
-        }
-
-        public function getNationality() {
-            return $this->nationality;
-        }
-
-        public function getUserInfo() {
-            echo 'First name: ' . $this->getFirstName() . '<br>';
-            echo 'Last name: ' . $this->getLastName() . '<br>';
-            echo 'Email: ' . $this->getEmail() . '<br>';
-            echo 'Age: ' . $this->getAge() . '<br>';
-            echo 'Gender: ' . $this->getGender() . '<br>';
-            echo 'Nationality: ' . $this->getNationality() . '<br>';
-        }
-    }
+    require 'User.php';
 
     $user1 = new User('Marco', 'Rossi', 'marco.rossi@gmail.com');
     echo '<h1>Utente 1:</h1>';
