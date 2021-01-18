201298bed0b946df92132059adb4de543506a2ae
Valerio Farina <valerio.farina.1994@gmail.com>
2021-01-18 15:11:47 +0100

    Create due istanze della classe User e stampate a schermo le relative proprietà

--- index.php
+++ index.php
@@ -50,5 +50,13 @@
         }
     }
 
+    $user1 = new User('Marco', 'Rossi', 'marco.rossi@gmail.com');
+    echo '<h1>Utente 1:</h1>';
+    $user1->getUserInfo();
+
+    $user2 = new User('Paolo', 'Verdi', 'paolo.verdi@hotmail.com', 30, 'M', 'IT');
+    echo '<h1>Utente 2:</h1>';
+    $user2->getUserInfo();
+
 
 ?>
